d452aadf87fa440cf51bc8b2160fad10acba9c51
lexciese <falah.naufal.zk@gmail.com>
2025-04-12 22:14:43 +0700

    update

--- packages/pure/schemas/social.ts
+++ packages/pure/schemas/social.ts
@@ -19,6 +19,7 @@ export const SocialLinksSchema = () =>
           github: 'GitHub',
           gitlab: 'GitLab',
           discord: 'Discord',
+          linkedin: 'LinkedIn',
           youtube: 'YouTube',
           instagram: 'Instagram',
           x: 'X',
