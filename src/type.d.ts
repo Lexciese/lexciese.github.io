bb12b1580104ca712ee28139ae9aa99c187254f8
lexciese <falah.naufal.zk@gmail.com>
2025-04-13 14:00:18 +0700

    update

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,3 +1,24 @@
+import { IntegrationUserConfig as BaseIntegrationUserConfig } from 'astro-pure/types'
+
+declare module 'astro-pure/types' {
+  interface GiscusConfig {
+    enable: boolean
+    repo: string
+    repoId: string
+    category: string
+    categoryId: string
+    mapping: string
+    reactionsEnabled: string
+    emitMetadata: string
+    inputPosition: string
+    lang: string
+  }
+
+  interface IntegrationUserConfig extends BaseIntegrationUserConfig {
+    giscus?: GiscusConfig
+  }
+}
+
 declare module 'virtual:config' {
   const Config: import('astro-pure/types').ConfigOutput
   export default Config
